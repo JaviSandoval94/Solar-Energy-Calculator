7355c712eb6b59bb31376ea534f04ae8706df5f3
elc17 <neto@lopezcenteno.com>
2020-08-05 19:39:16 -0600

    Funcionalidad de los selectores

--- static/js/solar.js
+++ static/js/solar.js
@@ -1,70 +1,90 @@
 url = "/api/solar"
-var data = d3.json(url)
-
-data.then(info => {
-    console.log(info)
-    // console.log(info[0])
-
-    var id = "1";
-    var eval = "sunHour";
-    var spec = "specific_prod(kWh/kWp)";
-    var pes = [];
-    var evalist = [];
-    var text = [];
-
-    if(eval == "sunHour"){
-        var x_label = "Sunlight Exposure [hours]";
-        var title = "Specific Production vs Sunlight Exposure";
-    }
-    else if(eval == "uvIndex"){
-        var x_label = "Ultraviolet Radiation Index";
-        var title = "Specific Production vs Ultraviolet Radiation Index";
-    }
-    else{
-        var x_label = "Cloud Coverage [%]";
-        var title = "Specific Production vs Cloud Coverage";
-    }
-
-    Object.entries(info).forEach(([key, value])=>{
-        if(value.id == id){
-            pes.push(value[spec])
-            evalist.push(value[eval])
-            text.push(value[spec], value[eval])
-        }
-    });
-
-    var bubble = d3.select("#bubble"); // Select the bubble chart
-    bubble.html("");  // Clear bubble chart HTML
-
-    var bData = [{ // Create the data for the chart
-        type: "scatter",
-        x: evalist,
-        y: pes,
-        text: text,
-        mode: "markers",
-        marker: {
-            size: 30,
-            color: evalist,
-            colorscale: "Viridis",          
-        },
-    }];
-
-    var bLayout = {
-        title: title,
-        // margin: {t: 0},
-        hovermode: "closest",
-        xaxis: {title: x_label},
-        yaxis: {title: " Specific Production [kWh/kWp]"},
-        // margin: {t: 30},
-        // plot_bgcolor: "#001845",
-        // paper_bgcolor:"#001233",
-        font: {
-            // color: "rgb(220,220,220)",
-        }
-    };
-
-    Plotly.newPlot("bubble", bData, bLayout);
-
-    // info.filter(selected=>selected.id=id)
-    
+var data = d3.json(url);
+var info;
+data.then(i =>{
+    info = i;
 });
+
+var plant = d3.select("#plant_id");
+var compare = d3.select("#comparison");
+// console.log(plant);
+
+plant.on("change", plotGraphs);
+compare.on("change", bubblePlot);
+
+function plotGraphs() {
+    var bubble = d3.select("#bubble"); // Select the bubble chart
+    var time = d3.select("#time"); // Select the time-series plot
+    bubble.html("");  // Clear bubble chart HTML
+    time.html(""); // Clear the time-series plot HTML
+
+    bubblePlot();
+
+// Preguntar si hay forma de hacerle "reset"
+    // compare.property("text", "-- Choose a variable");
+    // compare.property("value", "- Choose a variable");
+    // compare.property("selected", "-- Choose a variable");
+}
+
+function bubblePlot(){
+        // console.log(info)
+        // console.log(info[0])
+        var id = plant.property("value");
+        // console.log(id);
+        var eval = compare.property("value");
+        var spec = "specific_prod(kWh/kWp)";
+        var pes = [];
+        var evalist = [];
+        var text = [];
+
+        if(eval == "sunHour"){
+            var x_label = "Sunlight Exposure [hours]";
+            var title = "Specific Production vs Sunlight Exposure";
+        }
+        else if(eval == "uvIndex"){
+            var x_label = "Ultraviolet Radiation Index";
+            var title = "Specific Production vs Ultraviolet Radiation Index";
+        }
+        else if (eval =="cloudcover"){
+            var x_label = "Cloud Coverage [%]";
+            var title = "Specific Production vs Cloud Coverage";
+        }
+
+        Object.entries(info).forEach(([key, value])=>{
+            if(value.id == id){
+                pes.push(value[spec])
+                evalist.push(value[eval])
+                text.push(`Specific Production: ${value[spec]} | ${x_label}: ${value[eval]}`)
+            }
+        });
+
+        var bubble = d3.select("#bubble"); // Select the bubble chart
+        bubble.html("");  // Clear bubble chart HTML
+
+        var bData = [{ // Create the data for the chart
+            type: "scatter",
+            x: evalist,
+            y: pes,
+            text: text,
+            mode: "markers",
+            marker: {
+                size: 30,
+                color: evalist,
+                colorscale: "Viridis",          
+            },
+        }];
+
+        var bLayout = {
+            title: title,
+            hovermode: "closest",
+            xaxis: {title: x_label},
+            yaxis: {title: " Specific Production [kWh/kWp]"},
+            // plot_bgcolor: "#001845",
+            // paper_bgcolor:"#001233",
+            font: {
+                // color: "rgb(220,220,220)",
+            }
+        };
+
+        Plotly.newPlot("bubble", bData, bLayout);       
+}
